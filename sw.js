42cb9d90dfe5312cf1652ec46a1cddff99224a8a
BrendonBarela <71888997+BrendonBarela@users.noreply.github.com>
2026-09-14 16:38:39 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sera-brendon-wedding-v18";
+const CACHE_NAME = "sera-brendon-wedding-v19";
 const CORE_ASSETS = [
   "./","./index.html","./today.html","./packing.html","./essentials.html",
   "./verona.html","./parma.html","./ispra.html","./santa-margherita.html","./nice.html",
